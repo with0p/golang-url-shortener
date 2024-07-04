beeae26f7fdb24a2146e4f09f7f2611477d01803
Mariia Zhakova <mzhakova@tradingview.com>
2024-07-04 09:53:22 +0200

    Updates flag parser

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"net/url"
 )
 
 var CMDFlags struct {
@@ -9,8 +10,38 @@ var CMDFlags struct {
 	ShortURL string
 }
 
+const URL_HOST = "localhost"
+const BASE_PORT = "8080"
+const SHORT_PORT = "8888"
+
 func ParseCMDFlags() {
-	flag.StringVar(&CMDFlags.BaseURL, "a", "localhost:8080", "base URL")
-	flag.StringVar(&CMDFlags.ShortURL, "b", "localhost:8888", "short URL")
+	var BaseURL string
+	var ShortURL string
+
+	flag.StringVar(&BaseURL, "a", "http://localhost:8080", "base URL")
+	flag.StringVar(&ShortURL, "b", "http://localhost:8888", "short URL")
 	flag.Parse()
+
+	CMDFlags.BaseURL = URLParseHelper(BaseURL, BASE_PORT)
+	CMDFlags.ShortURL = URLParseHelper(ShortURL, SHORT_PORT)
+}
+
+func URLParseHelper(str string, defaultPort string) string {
+	parsedURL, _ := url.ParseRequestURI(str)
+
+	if parsedURL.Host == "" {
+		parsedURL, _ = url.ParseRequestURI("http://" + str)
+	}
+
+	port := parsedURL.Port()
+	if port == "" {
+		port = defaultPort
+	}
+
+	host := parsedURL.Hostname()
+	if host == "" {
+		host = defaultPort
+	}
+
+	return host + ":" + port
 }

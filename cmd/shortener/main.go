49420c10464153d6070013c59d5061cb50eee79d
Mariia Zhakova <mzhakova@tradingview.com>
2024-07-04 10:54:05 +0200

    Without second server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,12 +12,12 @@ func main() {
 
 	router := handlers.ServerRouter()
 
-	go func() {
-		err2 := http.ListenAndServe(config.CMDFlags.ShortURL, router)
-		if err2 != nil {
-			panic(err2)
-		}
-	}()
+	// go func() {
+	// 	err2 := http.ListenAndServe(config.CMDFlags.ShortURL, router)
+	// 	if err2 != nil {
+	// 		panic(err2)
+	// 	}
+	// }()
 
 	err1 := http.ListenAndServe(config.CMDFlags.BaseURL, router)
 	if err1 != nil {

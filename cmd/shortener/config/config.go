44b8cf238112ced77a63aeb4ea5fea3daa03354c
Mariia Zhakova <mzhakova@tradingview.com>
2024-07-05 23:55:59 +0200

    Adds envs

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -3,30 +3,39 @@ package config
 import (
 	"flag"
 	"net/url"
+	"os"
 )
 
-var CMDFlags struct {
+var Config struct {
 	BaseURL  string
 	ShortURL string
 }
 
 const defaultHost = "localhost"
-const defaultBasePort = "8080"
-const defaultShortPort = "8888"
+const defaultPort = "8080"
 
-func ParseCMDFlags() {
+func ParseConfig() {
 	var BaseURL string
 	var ShortURL string
 
-	flag.StringVar(&BaseURL, "a", defaultHost+":"+defaultBasePort, "base URL")
-	flag.StringVar(&ShortURL, "b", "http://"+defaultHost+":"+defaultBasePort, "short URL")
+	flag.StringVar(&BaseURL, "a", defaultHost+":"+defaultPort, "base URL")
+	flag.StringVar(&ShortURL, "b", "http://"+defaultHost+":"+defaultPort, "short URL")
 	flag.Parse()
 
-	CMDFlags.BaseURL = URLParseHelper(BaseURL, defaultBasePort)
-	CMDFlags.ShortURL = "http://" + URLParseHelper(ShortURL, defaultShortPort)
+	if envServerAddress := os.Getenv("SERVER_ADDRESS"); envServerAddress != "" {
+		BaseURL = envServerAddress
+	}
+
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		ShortURL = envBaseURL
+	}
+
+	Config.BaseURL = URLParseHelper(BaseURL)
+	Config.ShortURL = "http://" + URLParseHelper(ShortURL)
+
 }
 
-func URLParseHelper(str string, defaultPort string) string {
+func URLParseHelper(str string) string {
 	parsedURL, _ := url.ParseRequestURI(str)
 
 	if parsedURL.Host == "" {
--- cmd/shortener/handlers/handlers.go
+++ cmd/shortener/handlers/handlers.go
@@ -38,7 +38,7 @@ func URLShortener(res http.ResponseWriter, req *http.Request) {
 
 	res.Header().Set("content-type", "text/plain")
 	res.WriteHeader(201)
-	res.Write([]byte(config.CMDFlags.ShortURL + "/" + urlKey))
+	res.Write([]byte(config.Config.ShortURL + "/" + urlKey))
 }
 
 func GetTrueURL(res http.ResponseWriter, req *http.Request) {
